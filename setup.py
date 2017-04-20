e2b64d5d899ea9e3a0be8418e41bfa0f31f6ee58
Brandon Jurewicz <brandonjur@gmail.com>
2017-04-20 14:58:45 -0400

    version bump

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ if sys.argv[-1] == 'publish':
 
 setup(
     name='django-squeezemail',
-    version='0.11.4',
+    version='0.11.5',
     author=author,
     author_email=author_email,
     packages=get_packages(package),
